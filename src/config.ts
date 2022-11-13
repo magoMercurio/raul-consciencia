1368640fa35dcc54a83f0831d8e1f934dc415372
mago_d_Mercurio <davila.feria@gmail.com>
2022-11-12 19:13:08 -0600

    config index global css update

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Raul Consciencia';
-export const SITE_DESCRIPTION = 'Bienvenido al Blog de Raul';
+export const SITE_TITLE = 'Raúl Consciencia';
+export const SITE_DESCRIPTION = 'Bienvenido al Blog de Raulraul Consciencia';
